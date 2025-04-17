bfd04a6f9588ce68b2122f93e2a99a66b8fd22b9
LAPTOP-NIFDOGS3\sansg <cadenasu@shernet.sheridancollege.ca>
2025-04-17 01:59:13 -0400

    THEY MOVE ONE WAY

--- Assets/SystemProject-Assignment3/Scripts/SliderMarker.cs
+++ Assets/SystemProject-Assignment3/Scripts/SliderMarker.cs
@@ -8,19 +8,22 @@ public class SliderMarker : MonoBehaviour
 {
     public Slider sliderX;
     public Slider sliderY;
-    public float valueX;
-    public float valueY;
+    //public float valueX;
+    //public float valueY;
 
     void Start()
     {
-        valueX = sliderX.value;
-        valueY = sliderY.value;
+        //valueX = sliderX.value;
+        //valueY = sliderY.value;
         Debug.Log("Programa Empieza");
     }
 
     void Update()
     {
-        //Debug.Log("MarkerUpdate");
+        sliderX.value += 0.3f;
+        sliderY.value += 0.3f;
+        Debug.Log("MarkerUpdate" +" "+ Time.deltaTime +" "+ sliderX.value + " " + sliderY.value);
+
     }
 
     //VOID ON CLICK EVENT
