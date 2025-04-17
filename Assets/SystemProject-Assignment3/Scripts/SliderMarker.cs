993be09af37165601e0ff0f9832cd815efe91b15
LAPTOP-NIFDOGS3\sansg <cadenasu@shernet.sheridancollege.ca>
2025-04-17 11:29:44 -0400

    BACKUP - GONNA TEST SOMETHING QUICK

--- Assets/SystemProject-Assignment3/Scripts/SliderMarker.cs
+++ Assets/SystemProject-Assignment3/Scripts/SliderMarker.cs
@@ -20,10 +20,17 @@ public class SliderMarker : MonoBehaviour
 
     void Update()
     {
-        sliderX.value += 0.3f; //INCREASE RATE FOR THE VALUE OF SLIDER X
-        sliderY.value += 0.3f; //INCREASE RATE FOR THE VALUE OF SLIDER Y
-        Debug.Log("MarkerUpdate" +" "+ Time.deltaTime +" "+ sliderX.value + " " + sliderY.value); //DEBUG LOG TO SHOW ME WHEN THIS SCRIPT HAPPENS DURING UPDATE, AS WELL AS DELTA TIME AND MY SLIDER VALUES
-        //IF STATEMENTS TO MAKE THE SLIDER VALUES NEGATIVE
+        if (sliderX.value < 8 & sliderY.value < 8) //IF STATEMENTS TO MAKE THE SLIDER VALUES POSITIVE
+        {
+            sliderX.value += 0.3f; //INCREASE RATE FOR THE VALUE OF SLIDER X
+            sliderY.value += 0.3f; //INCREASE RATE FOR THE VALUE OF SLIDER Y
+        } 
+        else if (sliderX.value >= 8 & sliderY.value >= 8) //IF STATEMENTS TO MAKE THE SLIDER VALUES NEGATIVE
+        {
+            sliderX.value += -0.3f; //DECREASE RATE FOR THE VALUE OF SLIDER X
+            sliderY.value += -0.3f; //DECREASE RATE FOR THE VALUE OF SLIDER Y
+        }
+            Debug.Log("MarkerUpdate" + " " + Time.deltaTime + " " + sliderX.value + " " + sliderY.value); //DEBUG LOG TO SHOW ME WHEN THIS SCRIPT HAPPENS DURING UPDATE, AS WELL AS DELTA TIME AND MY SLIDER VALUES
     }
 
     //VOID ON CLICK EVENT
