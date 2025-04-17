4e0aa1b51d77877a89f8412d8edc41fd548d6359
LAPTOP-NIFDOGS3\sansg <cadenasu@shernet.sheridancollege.ca>
2025-04-17 12:13:00 -0400

    will come back to this

--- Assets/SystemProject-Assignment3/Scripts/SliderMarker.cs
+++ Assets/SystemProject-Assignment3/Scripts/SliderMarker.cs
@@ -8,6 +8,7 @@ public class SliderMarker : MonoBehaviour
 {
     public Slider sliderX; //SLIDER X OBJECT VARIABLE
     public Slider sliderY; //SLIDER Y OBJECT VARIABLE
+    //private IEnumerator markerMovement;
     //public float valueX;
     //public float valueY;
 
@@ -24,17 +25,34 @@ public class SliderMarker : MonoBehaviour
         {
             sliderX.value += 0.3f; //INCREASE RATE FOR THE VALUE OF SLIDER X
             sliderY.value += 0.3f; //INCREASE RATE FOR THE VALUE OF SLIDER Y
-        } 
+        }
         else if (sliderX.value >= 8 & sliderY.value >= 8) //IF STATEMENTS TO MAKE THE SLIDER VALUES NEGATIVE
         {
             sliderX.value += -0.3f; //DECREASE RATE FOR THE VALUE OF SLIDER X
             sliderY.value += -0.3f; //DECREASE RATE FOR THE VALUE OF SLIDER Y
         }
-            Debug.Log("MarkerUpdate" + " " + Time.deltaTime + " " + sliderX.value + " " + sliderY.value); //DEBUG LOG TO SHOW ME WHEN THIS SCRIPT HAPPENS DURING UPDATE, AS WELL AS DELTA TIME AND MY SLIDER VALUES
+        //markerMovement = MarkerMovement();
+        //StartCoroutine(markerMovement);
+
+        Debug.Log("MarkerUpdate" + " " + Time.deltaTime + " " + sliderX.value + " " + sliderY.value); //DEBUG LOG TO SHOW ME WHEN THIS SCRIPT HAPPENS DURING UPDATE, AS WELL AS DELTA TIME AND MY SLIDER VALUES
     }
 
     //VOID ON CLICK EVENT
         //MARK X COORD
         //MARK Y COORD
         //DETERMINE SCORE INCREASE //IF STATEMENTS THAT STATE HOW MUCH THE SCORE INCREASES DEPENDING ON THE SLIDER VALUES ("X & Y CO-ORDINATES")
+
+//private IEnumerator MarkerMovement()
+//    {
+//        if (sliderX.value < 8 & sliderY.value < 8) //IF STATEMENTS TO MAKE THE SLIDER VALUES POSITIVE
+//        {
+//            sliderX.value += 0.3f; //INCREASE RATE FOR THE VALUE OF SLIDER X
+//            sliderY.value += 0.3f; //INCREASE RATE FOR THE VALUE OF SLIDER Y
+//        }
+//        else if (sliderX.value >= 8 & sliderY.value >= 8) //IF STATEMENTS TO MAKE THE SLIDER VALUES NEGATIVE
+//        {
+//            sliderX.value += -0.3f; //DECREASE RATE FOR THE VALUE OF SLIDER X
+//            sliderY.value += -0.3f; //DECREASE RATE FOR THE VALUE OF SLIDER Y
+//        }
+//    }
 }
