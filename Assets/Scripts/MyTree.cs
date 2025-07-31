ca9108d6f77633d30b142bd9c872a6ff27e23f1f
Ramon Cadenas Urdaneta <cadenasu@shernet.sheridancollege.ca>
2025-07-31 19:24:30 -0400

    completed tree and apple growing on button press mechanic among other exercises

---
+++ Assets/Scripts/MyTree.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyTree : MonoBehaviour
+{
+    public AnimationCurve curve;
+
+    public float duration;
+
+    private float currentTime = 0f;
+
+    public List<Transform> appleTransforms;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+
+    public void StartGrow()
+    {
+        currentTime = 0f;
+
+        StartCoroutine(Grow());
+    }
+
+    public IEnumerator Grow()
+    {
+        while (currentTime < duration)
+        {
+            currentTime += Time.deltaTime;
+
+            float timeRatio = currentTime / duration;
+
+            transform.localScale = Vector3.one * curve.Evaluate(timeRatio);
+
+            yield return null;
+        }
+
+        currentTime = 0f;
+
+        int i = 0;
+
+        while (currentTime < duration && i <appleTransforms.Count)
+        {
+            currentTime += Time.deltaTime;
+
+            float timeRatio = currentTime / duration;
+                
+                appleTransforms[i].localScale = Vector3.one * curve.Evaluate(timeRatio);
+                
+           
+
+            yield return null;
+
+            if(currentTime >= duration)
+            {
+                i++;
+                currentTime = 0f;
+            }
+        }
+
+
+
+    }
+}
