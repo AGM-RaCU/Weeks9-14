c37292fb69cf6d6d0c8ae017cd33e9bb49487e44
LAPTOP-NIFDOGS3\sansg <cadenasu@shernet.sheridancollege.ca>
2025-08-12 15:53:51 -0400

    Working on GO button

--- Assets/RaCU's Assignment 3 (Scene&Assets)/Scripts/PlateMoverRaCU.cs
+++ Assets/RaCU's Assignment 3 (Scene&Assets)/Scripts/PlateMoverRaCU.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PlateMoverRaCU : MonoBehaviour
 {
+
+    public UnityEvent goButtonPressed;
+
     // Start is called before the first frame update
     void Start()
     {
