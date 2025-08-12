f21035a7531cc93f736e249b5357dca0266bbf66
LAPTOP-NIFDOGS3\sansg <cadenasu@shernet.sheridancollege.ca>
2025-08-12 15:23:03 -0400

    Basic button functionality implemented to spawn toppings, changed to prefabs because its easy

--- Assets/RaCU's Assignment 3 (Scene&Assets)/Scripts/ToppingSpawnerRaCU.cs
+++ Assets/RaCU's Assignment 3 (Scene&Assets)/Scripts/ToppingSpawnerRaCU.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class ToppingSpawnerRaCU : MonoBehaviour
 {
+    public GameObject TomatoPrefab;
+    public GameObject OnionPrefab;
+    public GameObject LettucePrefab;
+    public GameObject CilantroPrefab;
+    public GameObject SaucePrefab;
+    public GameObject HotSaucePrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +21,35 @@ public class ToppingSpawnerRaCU : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void placeTomato()
+    {
+        Instantiate(TomatoPrefab);
+    }
+
+    public void placeOnion()
+    {
+        Instantiate(OnionPrefab);
+    }
+
+    public void placeLettuce()
+    {
+        Instantiate(LettucePrefab);
+    }
+
+    public void placeCilantro()
+    {
+        Instantiate(CilantroPrefab);
+    }
+
+    public void placeSauce()
+    {
+        Instantiate(SaucePrefab);
+    }
+
+    public void placeHotSauce()
+    {
+        Instantiate(HotSaucePrefab);
     }
 }
