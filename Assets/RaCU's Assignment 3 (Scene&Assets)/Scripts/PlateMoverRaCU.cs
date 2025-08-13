09b72d0f1a77aa5c23bc062a42367a3913d12500
LAPTOP-NIFDOGS3\sansg <cadenasu@shernet.sheridancollege.ca>
2025-08-13 14:03:02 -0400

    First draft done

--- Assets/RaCU's Assignment 3 (Scene&Assets)/Scripts/PlateMoverRaCU.cs
+++ Assets/RaCU's Assignment 3 (Scene&Assets)/Scripts/PlateMoverRaCU.cs
@@ -6,21 +6,20 @@ namespace RaCU
 {
     public class PlateMoverRaCU : MonoBehaviour
     {
-        public GameObject spawner;
-        public UnityEvent goButtonPressed;
+        public GameObject spawner; //spawner gameobject
+        public UnityEvent goButtonPressed; //event for button press
 
-        public void buttonPressed()
+        public void buttonPressed() //method for button press
         {
-            StartCoroutine(movePlate());
-            goButtonPressed.Invoke();
+            StartCoroutine(movePlate());//start coroutine
+            goButtonPressed.Invoke();//call event
         }
 
-        public IEnumerator movePlate()
+        public IEnumerator movePlate() //coroutine to move plate
         {
             Vector3 platePosition = transform.position + Vector3.down * 5f; //vector3 defining how the plate will move down
             transform.position = platePosition; //applies vector3 to plate's transform position, plate moves
-            yield return null;
-            Debug.Log("Coroutine started");
+            yield return null; //end coroutine
             
         }
     }
--- Assets/RaCU's Assignment 3 (Scene&Assets)/Scripts/ToppingSpawnerRaCU.cs
+++ Assets/RaCU's Assignment 3 (Scene&Assets)/Scripts/ToppingSpawnerRaCU.cs
@@ -5,53 +5,47 @@ namespace RaCU
 {
     public class ToppingSpawnerRaCU : MonoBehaviour
     {
-        public GameObject TomatoPrefab;
-        public GameObject OnionPrefab;
-        public GameObject LettucePrefab;
-        public GameObject CilantroPrefab;
-        public GameObject SaucePrefab;
-        public GameObject HotSaucePrefab;
-        public GameObject Plate;
+        public GameObject TomatoPrefab, OnionPrefab, LettucePrefab, CilantroPrefab, SaucePrefab, HotSaucePrefab, Plate; //nacho plate and toppings
 
-        public void placeTomato()
+        public void placeTomato()//method to place tomato
         {
-            GameObject Tomato = Instantiate(TomatoPrefab);
-            Tomato.transform.parent = transform;
+            GameObject Tomato = Instantiate(TomatoPrefab);//spawn tomato prefab
+            Tomato.transform.parent = transform;//make prefab child of spawner object
         }
 
-        public void placeOnion()
+        public void placeOnion()//method to place onion
         {
-            GameObject Onion = Instantiate(OnionPrefab);
-            Onion.transform.parent = transform;
+            GameObject Onion = Instantiate(OnionPrefab);//spawn onion prefab
+            Onion.transform.parent = transform;//make prefab child of spawner object
         }
 
-        public void placeLettuce()
+        public void placeLettuce()//method to place lettuce
         {
-            GameObject Lettuce = Instantiate(LettucePrefab);
-            Lettuce.transform.parent = transform;
+            GameObject Lettuce = Instantiate(LettucePrefab);//spawn lettuce prefab
+            Lettuce.transform.parent = transform;//make prefab child of spawner object
         }
 
-        public void placeCilantro()
+        public void placeCilantro()//method to place cilantro
         {
-            GameObject Cilantro = Instantiate(CilantroPrefab);
-            Cilantro.transform.parent = transform;
+            GameObject Cilantro = Instantiate(CilantroPrefab);//spawn cilantro prefab
+            Cilantro.transform.parent = transform;//make prefab child of spawner object
         }
 
-        public void placeSauce()
+        public void placeSauce()//method to place sauce
         {
-            GameObject Sauce = Instantiate(SaucePrefab);
-            Sauce.transform.parent = transform;
+            GameObject Sauce = Instantiate(SaucePrefab);//spawn sauce prefab
+            Sauce.transform.parent = transform;//make prefab child of spawner object
         }
 
-        public void placeHotSauce()
+        public void placeHotSauce()//method to place hot sauce
         {
-            GameObject HotSauce = Instantiate(HotSaucePrefab);
-            HotSauce.transform.parent = transform;
+            GameObject HotSauce = Instantiate(HotSaucePrefab);//spawn hot sauce prefab
+            HotSauce.transform.parent = transform;//make prefab child of spawner object
         }
 
-        public void plateConnect()
+        public void plateConnect()//method to have spawner follow plate (called on button press event from other script)
         {
-            transform.position = Plate.transform.position;
+            transform.position = Plate.transform.position;//set spawner position to plate position
         }
     }
 }
